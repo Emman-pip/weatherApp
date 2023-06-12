b7e12d58039dd1dc7f1112b23fae2643da7b3e9b
Emman-pip <115330041+Emman-pip@users.noreply.github.com>
2023-06-12 18:31:16 +0800

    Replace weather function with forecast

--- source-files/script.js
+++ source-files/script.js
@@ -2,7 +2,6 @@
 function oneMonthAgo() {
   const date = new Date();
   const day = () => {
-    console.log(date.getDate().toString().length);
     if (date.getDate().toString.length == 1) {
       return `0${date.getDate()}`;
     } else {
@@ -30,39 +29,54 @@ async function APIdata(location) {
       }
     );
 
-    const [dataWeather, dataNews] = await Promise.all([
-      fetch(
-        `https://api.weatherapi.com/v1/current.json?key=31c3bfca6ae74f2bacb123625230806&q=${location}`,
-        {
-          mode: "cors",
-        }
-      ),
+    const [dataNews, dataForecast] = await Promise.all([
       fetch(
         `https://newsapi.org/v2/everything?q=weather-${location}&from=${oneMonthAgo()}&sortBy=popularity&apiKey=b2950f13dd814fa19bd4a6e883ef3217`,
         {
           mode: "cors",
         }
       ),
+      fetch(
+        `https://api.weatherapi.com/v1/forecast.json?key=31c3bfca6ae74f2bacb123625230806&q=${location}`,
+        {
+          mode: "cors",
+        }
+      ),
     ]);
 
-    const jsn = await Promise.all([dataWeather.json(), dataNews.json()]);
+    const jsn = await Promise.all([dataNews.json(), dataForecast.json()]);
     return jsn;
   } catch (error) {
     console.log(error);
   }
 }
 
-//weather api
-async function weatherData(location) {
+//forecast
+async function forecastData(location) {
   const data = await APIdata(location);
-  const condition = data[0].current.condition.text;
-  const countryCity = `${data[0].location.name}, ${data[0].location.country}`;
-  return [countryCity, condition];
+  //   //location
+  //   const city = data[1].location.name;
+  //   const country = data[1].location.country;
+  //   //date of forecast
+  //   const date = data[1].forecast.forecastday[0].date;
+  //   //per hour
+  //   const forecast = data[1].forecast.forecastday[0].hour;
+  //   const at_1am = data[1].forecast.forecastday[0].hour[1].temp_c;
+  //   const at_3am = data[1].forecast.forecastday[0].hour[1].temp_c;
+  //   const at_6am = data[1].forecast.forecastday[0].hour[1].temp_c;
+  //   const at_9am = data[1].forecast.forecastday[0].hour[1].temp_c;
+  //   const at_12am = data[1].forecast.forecastday[0].hour[1].temp_c;
+  //   const at_1pm = data[1].forecast.forecastday[0].hour[1].temp_c;
+  //   const at_3pm = data[1].forecast.forecastday[0].hour[1].temp_c;
+  //   const at_6pm = data[1].forecast.forecastday[0].hour[1].temp_c;
+  //   const at_9pm = data[1].forecast.forecastday[0].hour[1].temp_c;
+  //   const at_12pm = data[1].forecast.forecastday[0].hour[1].temp_c;
+
+  //   console.log(data[1]);
+  //   console.log(at_12am);
+  return data[1];
 }
-async function weather(country) {
-  const lol = await weatherData(country);
-  console.log(`WEATHER:\nlocation: ${lol[0]}\ncondition:${lol[1]}`);
-}
+forecastData("Batangas");
 
 //news
 function keyWords(subject) {
@@ -94,15 +108,15 @@ function keyWords(subject) {
 
 function toAppend(data, list1, list2, list3) {
   //title
-  for (let i = 0; i < data[1].articles.length; i++) {
-    const titles = data[1].articles[i].title;
+  for (let i = 0; i < data[0].articles.length; i++) {
+    const titles = data[0].articles[i].title;
     list2.push(titles);
     //description
-    const preDescriptions = data[1].articles[i].description;
+    const preDescriptions = data[0].articles[i].description;
     list1.push(preDescriptions);
 
     //url
-    const urls = data[1].articles[i].url;
+    const urls = data[0].articles[i].url;
     list3.push(urls);
   }
 }
@@ -110,7 +124,6 @@ function toAppend(data, list1, list2, list3) {
 //for news
 // TODO: capitalize the 1st letter[of "subject"] and lowercase the remaining
 async function newsData(subject) {
-  console.log(subject);
   const data = await APIdata(subject);
   const articleTitle = [];
   const articleDescription = [];
@@ -135,6 +148,12 @@ async function newsData(subject) {
 
   return [articleTitle, articleDescription, articleUrl];
 }
+
+// async function weather(country) {
+//   const lol = await weatherData(country);
+//   console.log(`WEATHER:\nlocation: ${lol[0]}\ncondition:${lol[1]}`);
+// }
+
 newsData("Philippines").then((res) => {
   console.log(res);
 });
