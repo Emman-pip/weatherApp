b2383a9082296648c0e5c9157d8f37e200ff7fc3
Emman-pip <115330041+Emman-pip@users.noreply.github.com>
2023-06-12 13:52:44 +0800

    Add weatherData(returns/logs weather condition of the place entered)

--- source-files/script.js
+++ source-files/script.js
@@ -1,1 +1,14 @@
-console.log("hello world");
+async function weatherData(location) {
+  const data = await fetch(
+    `https://api.weatherapi.com/v1/current.json?key=31c3bfca6ae74f2bacb123625230806&q=${location}`,
+    {
+      mode: "cors",
+    }
+  );
+  const jsn = await data.json();
+  const condition = jsn.current.condition.text;
+  console.log(condition);
+  return condition;
+}
+
+weatherData("sahara dessert");
