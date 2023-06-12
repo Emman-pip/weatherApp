302d93fa6d603a8e4911135cefa94901d457c39f
Emman-pip <115330041+Emman-pip@users.noreply.github.com>
2023-06-12 17:18:39 +0800

    Add a function returning data regarding the location(articles' title, url, and description).

--- source-files/script.js
+++ source-files/script.js
@@ -1,4 +1,26 @@
-//weather api
+// date
+function oneMonthAgo() {
+  const date = new Date();
+  const day = () => {
+    console.log(date.getDate().toString().length);
+    if (date.getDate().toString.length == 1) {
+      return `0${date.getDate()}`;
+    } else {
+      return date.getMonth();
+    }
+  };
+  const month = () => {
+    if (date.getMonth().toString().length == 1) {
+      return `0${date.getMonth()}`;
+    } else {
+      return date.getMonth();
+    }
+  };
+  const year = date.getFullYear();
+
+  return `${year}-${month() - 1}-${day()}`;
+}
+//fetching shit
 async function APIdata(location) {
   try {
     const data = await fetch(
@@ -16,7 +38,7 @@ async function APIdata(location) {
         }
       ),
       fetch(
-        `https://newsapi.org/v2/everything?q=weather-${location}&from=2023-06-1&sortBy=popularity&apiKey=b2950f13dd814fa19bd4a6e883ef3217`,
+        `https://newsapi.org/v2/everything?q=weather-${location}&from=${oneMonthAgo()}&sortBy=popularity&apiKey=b2950f13dd814fa19bd4a6e883ef3217`,
         {
           mode: "cors",
         }
@@ -24,13 +46,13 @@ async function APIdata(location) {
     ]);
 
     const jsn = await Promise.all([dataWeather.json(), dataNews.json()]);
-    //    console.log(jsn);
     return jsn;
   } catch (error) {
     console.log(error);
   }
 }
 
+//weather api
 async function weatherData(location) {
   const data = await APIdata(location);
   const condition = data[0].current.condition.text;
@@ -41,32 +63,82 @@ async function weather(country) {
   const lol = await weatherData(country);
   console.log(`WEATHER:\nlocation: ${lol[0]}\ncondition:${lol[1]}`);
 }
-weather("london");
+
+//news
+function keyWords(subject) {
+  const list = [
+    subject,
+    "weather",
+    "rain",
+    "cloud",
+    "rainbow",
+    "temperature",
+    "pressure",
+    "overcast",
+    "shower",
+    "sunrise",
+    "dry",
+    "tornado",
+    "sunset",
+    "humidity",
+    "cold",
+    "heat",
+    "wind",
+    "cloudy",
+    "heat wave",
+    "cyclone",
+    "typhoon",
+  ];
+  return list;
+}
+
+function toAppend(data, list1, list2, list3) {
+  //title
+  for (let i = 0; i < data[1].articles.length; i++) {
+    const titles = data[1].articles[i].title;
+    list2.push(titles);
+    //description
+    const preDescriptions = data[1].articles[i].description;
+    list1.push(preDescriptions);
+
+    //url
+    const urls = data[1].articles[i].url;
+    list3.push(urls);
+  }
+}
 
 //for news
+// TODO: capitalize the 1st letter[of "subject"] and lowercase the remaining
 async function newsData(subject) {
+  console.log(subject);
   const data = await APIdata(subject);
-  console.log(data);
-  let num = 0;
-  //title
-  const articleTitle = data[1].articles[num].title;
-
-  //description
+  const articleTitle = [];
   const articleDescription = [];
-  const preDescription = data[1].articles.description;
+  const articleUrl = [];
 
-  //url
-  const articleUrl = data[1].articles.url;
+  const descriptions = [];
+  const titles = [];
+  const urls = [];
+  toAppend(data, descriptions, titles, urls);
 
-  console.log(articleTitle);
+  const filter = keyWords(subject);
+  for (let i = 0; i < filter.length; i++) {
+    titles.forEach((word) => {
+      if (word.includes(filter[i])) {
+        const index = titles.indexOf(word);
+        articleTitle.push(titles[index]);
+        articleDescription.push(descriptions[index]);
+        articleUrl.push(urls[index]);
+      }
+    });
+  }
+
+  return [articleTitle, articleDescription, articleUrl];
 }
-newsData("london weather");
-
-// return articleTitle;
+newsData("Philippines").then((res) => {
+  console.log(res);
+});
 
 // news api key:b2950f13dd814fa19bd4a6e883ef3217
 // url format `https://newsapi.org/v2/everything?q=weather-${location}&from=2023-06-1&sortBy=popularity&apiKey=b2950f13dd814fa19bd4a6e883ef3217`
 // `https://newsapi.org/v2/everything?q=weather-${location}&from=2023-06-1&sortBy=popularity&apiKey=b2950f13dd814fa19bd4a6e883ef3217`;
-
-// const lol = "Philippines";
-// weatherData(lol).then((res) => alert(res));
